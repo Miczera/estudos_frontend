9c60cefcc035f8051891c53ebc1c989b1363be09
Michel Rodrigues <micheldsouzarodrigues@gmail.com>
2023-10-19 03:45:35 -0300

    iniciando o exercício

--- Estudos_JavaScript/Aulas_OBC/index.js
+++ Estudos_JavaScript/Aulas_OBC/index.js
@@ -1,5 +1,12 @@
 let name = 'Michel'
 let idade = 22
 
-console.log(name);
-console.log(idade)
+console.log(name)
+console.error(name)
+console.warn(name)
+
+//window.alert(name)
+//name = window.prompt("insira o seu nome:")
+let confirma = window.confirm("deseja adicionar no carrinho?")
+
+console.log(confirma)
---
+++ Estudos_JavaScript/Excercicio_1_JS_I/ex.js
@@ -0,0 +1,12 @@
+let primeiroNome = window.prompt("Insira seu primeiro nome:")
+
+let sobrenome = window.prompt("Insira seu sobrenome:")
+
+let campoDeEstudo = window.prompt("Insira seu Campo de estudo:")
+
+let anoDeNascimento = window.prompt("insira seu ano de nascimento: ")
+
+console.log('Primeiro nome:' primeiroNome)
+console.log('sobrenome:' sobrenome)
+console.log('Campo de estudo:' campoDeEstudo)
+console.log('ano de nascimento:' anoDeNascimento)
